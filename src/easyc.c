1c22a3cb7ba30af1d45c58a40dd56d43efcc134d
Cory (Riley) Black <32332593+RileyMeta@users.noreply.github.com>
2025-04-29 21:31:57 -0400

    Update easyc.c
    +- Switched from getchar to fgets for more secure input
    + Added proper "TODO" statements to each nonfunctional function

--- src/easyc.c
+++ src/easyc.c
@@ -15,9 +15,10 @@ void restart(int current);
 char *get_location(int current, char *location);
 int get_current_exercise(void);
 int get_latest_exercise(void);
+void trim_newline(char *str);
 
 // Command Line Arguments
-void help_menu(void);
+void help_menu(int version);
 void version_info(void);
 void initialize_exercises(char *file);
 void clean_exercises(void);
@@ -34,16 +35,18 @@ typedef struct {
 } WatcherArgs;
 
 int main(int argc, char* argv[]) {
+    int running = 1;
+    char input[256];
     int exercises = 22; // TODO: Make this dynamic to all tests in exercises.config
     char exercise[256] = "00_intro";
     int current_exercise = get_current_exercise();
     int latest_exercise = get_latest_exercise();
     char location[256];
-    char response;
 
+    // CLI Arguments
     if (argc > 1) {
         if (strcmp(argv[1], "--help") == 0) {
-            help_menu();
+            help_menu(0);
             return 0;
         } else if (strcmp(argv[1], "-V") == 0 || strcmp(argv[1], "--version") == 0) {
             version_info();
@@ -81,30 +84,38 @@ int main(int argc, char* argv[]) {
         }
     }
 
-    get_location(1, location);
-    print_status(current_exercise, exercises, location);
-
-    print_options();
-
     do {
-        response = getchar();
+        get_location(1, location);
+        print_status(current_exercise, exercises, location);
 
-        if (response == 'n') {
-            next(++current_exercise);
-        } else if (response == 'b') {
-            back(--current_exercise);
-        } else if (response == 'h') {
-            show_hint(current_exercise);
-        } else if (response == 'r') {
-            restart(current_exercise);
-        } else if (response == 'q') {
-            printf("Exiting...");
-            return 0;
-        } else {
-            printf("Invalid Input: %s not recognized", response);
+        printf("Placeholder for Makefile/binary output\n");
+
+        print_options();
+
+        if (fgets(input, sizeof(input), stdin)) {
+            trim_newline(input);
+
+            if (strcmp(input, "n") == 0) {
+                printf("WORKING!\n");
+                break;
+            } else if (strcmp(input, "b") == 0) {
+                printf("Working!\n");
+                break;
+            } else if (strcmp(input, "h") == 0) {
+                printf("Hints Working!\n");
+                break;
+            } else if (strcmp(input, "q") == 0) {
+                printf("Quitting...\n");
+                running = 0;
+            } else if (strcmp(input, "r") == 0) {
+                printf("Restarting %s\n", exercise);
+            } else if (strcmp(input, "help") == 0) {
+                help_menu(2);
+            } else {
+                printf("Invalid Input: %s not recongnized\n", input);
+            }
         }
-    } while (1);
-
+    } while (running);
     return 0;
 }
 
@@ -119,21 +130,32 @@ void print_options(void) {
     printf("(h) Hint, (b) Back / (n) Next, (r) Reset, (q) Quit\n");
 }
 
+void trim_newline(char *str) {
+    size_t len = strlen(str);
+    if (len > 0 && str[len - 1] == '\n') {
+        str[len - 1] = '\0';
+    }
+}
+
 // Command Line Arguments
-void help_menu(void) {
-    printf("EasyC is a collection of small exercises to get you used to writing and reading C code\n");
-    printf("\n\033[4mUsage:\033[0m easyc [OPTIONS] [COMMAND]\n");
-    printf("\n\033[4mCommands:\033[0m\n");
-    printf("  init       Initialize the official EasyC exercises\n");
-    printf("  run        Run a single exercise. Runs the next pending exercise if the exercise name is not specified\n");
-    printf("  check-all  Check all the exercises, marking them as done or pending accordingly\n");
-    printf("  reset      Reset a single exercise\n");
-    printf("  hint       Show a hint. Shows the hint of the next pending exercise if the exercise name is not specified\n");
-    printf("  help       Print this message or the help of the given subcommand(s)\n");
-    printf("\n\033[4mOptions:\033[0m\n");
-    printf("      --manual-run  Manually run the current exercise using `r` in the watch mode. Only use this if EasyC fails to detect exercise file changes\n");
-    printf("  -h, --help        Prints this help menu\n");
-    printf("  -V, --version     Print version\n");
+void help_menu(int version) {
+    if (version == 0) {
+        printf("EasyC is a collection of small exercises to get you used to writing and reading C code\n");
+        printf("\n\033[4mUsage:\033[0m easyc [OPTIONS] [COMMAND]\n");
+        printf("\n\033[4mCommands:\033[0m\n");
+        printf("  init       Initialize the official EasyC exercises\n");
+        printf("  run        Run a single exercise. Runs the next pending exercise if the exercise name is not specified\n");
+        printf("  check-all  Check all the exercises, marking them as done or pending accordingly\n");
+        printf("  reset      Reset a single exercise\n");
+        printf("  hint       Show a hint. Shows the hint of the next pending exercise if the exercise name is not specified\n");
+        printf("  help       Print this message or the help of the given subcommand(s)\n");
+        printf("\n\033[4mOptions:\033[0m\n");
+        printf("      --manual-run  Manually run the current exercise using `r` in the watch mode. Only use this if EasyC fails to detect exercise file changes\n");
+        printf("  -h, --help        Prints this help menu\n");
+        printf("  -V, --version     Print version\n");
+    } else {
+        printf("Help Menu:\n");
+    }
 }
 
 void version_info(void) {
@@ -141,45 +163,46 @@ void version_info(void) {
 }
 
 void initialize_exercises(char *file) {
-    // Pull a fresh copy of the exercises with git
+    // TODO: Pull a fresh copy of the exercises with git
     // Use argv[2] (file pointer) as folder choice
 }
 
 void clean_exercises() {
-    // Pull a fresh copy of the exercises with git to argv[2] location
+    // TODO: Pull a fresh copy of the exercises with git to argv[2] location
 }
 
 void run_exercise(int current_exercise) {
-    // Run the Makefile of a specific exercise
+    // TODO: Run the Makefile of a specific exercise
 }
 
 void check_all(void) {
-    // Check every exercise for completion
+    // TODO: Check every exercise for completion
     // Change config: 1 = completed, 0 = incomplete
 }
 
 void reset(char *exercise) {
-    // Restore the exercise to it's Original version
+    // TODO: Restore the exercise to it's Original version
 }
 
 void restart(int current) {
-    // restart the current activity, restore to original (pull single from git?)
+    // TODO: restart the current activity, restore to original (pull single from git?)
 }
 
 void show_hint(int exercise) {
-    // Display the current.hint from exercises.config
+    // TODO: Display the current.hint from exercises.config
 }
 
 void next(int current) {
-    // next(++current): Advance to the next exercise IF and only if the user has completed or acknowledged
+    // TODO: next(++current): Advance to the next exercise IF and only if the user has completed or acknowledged
 }
 
 void back(int current) {
-    // back(--current): Show the previous exercise
+    // TODO: back(--current): Show the previous exercise
 }
 
 int get_current_exercise(void) {
-    // Return the active exercise number or name
+    // TODO: Return the active exercise number or name
+    return 1;
 }
 
 int get_latest_exercise(void) {
@@ -187,5 +210,5 @@ int get_latest_exercise(void) {
 }
 
 char *get_location(int current, char *location) {
-    // Display the location (in location[256]) for the exercise itself in the Taskbar
+    // TODO: Display the location (in location[256]) for the exercise itself in the Taskbar
 }
